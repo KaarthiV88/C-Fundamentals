b0078bf61e94f268fbfbfee488830fe71b4a04b3
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2024-03-09 17:40:25 -0600

    Add files via upload

---
+++ 1_Variables_and_Print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "John";
+    int characterAge = 35;
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+
+    characterName = "Kaarthi";
+    cout << "He liked the name " << characterName << " more however"<< endl;
+    cout << "But did not like being " << characterAge << endl;
+
+    return 0;
+}
