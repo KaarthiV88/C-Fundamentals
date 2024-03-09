8d480ceff986906c25b46a6132cff9fe7f7f0476
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2024-03-09 17:40:59 -0600

    Add files via upload

---
+++ 2_Data_Types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A+';
+    string school = "Vista Ridge High School";
+    int wholeGrade = 100;
+    //float GPA = 5.55;
+    double GPA = 5.506;
+    bool topTen = false;
+
+    cout << "Kaarthi Vedere: " << school << " -> " << wholeGrade << "(" << grade << ")" << GPA << endl;
+
+
+
+    return 0;
+}
