0267ede400b9ab7a7e39af878a8d333aab08ce1c
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2024-03-10 14:09:14 -0500

    Create Getting_User_Input.cpp

---
+++ 5) Getting User Input/Getting_User_Input.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "You are " << age << " years old" << endl;
+
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "\nHello, " << name << endl;
+
+
+    return 0;
+
+}
