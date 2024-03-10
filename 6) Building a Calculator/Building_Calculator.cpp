8d91eb63d82e72e933fa8bf8d5b503bcb46c7f54
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2024-03-10 14:16:07 -0500

    Create Building_Calculator.cpp
    Very basic calculator, just adding by testing user input.
    
    No if statement, so only one operation

---
+++ 6) Building a Calculator/Building_Calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    //int num1;
+    //int num2;
+    //=
+    int num1, num2;
+    cout << "Enter your first number: ";
+    cin >> num1;
+
+    cout << "/nEnter your second number: ";
+    cin >> num2;
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2;
+
+
+    return 0;
+
+}
